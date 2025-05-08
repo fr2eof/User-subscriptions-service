8755ae865a8ea5cb6d264862fa9de523ec3ba2b6
Ivanov Ilya <iiilya12@yandex.ru>
2025-05-08 19:27:19 +0300

    feat: add subs crud & top stats service layer

--- src/main/java/com/test/usersubscriptionsservice/repository/SubscriptionRepository.java
+++ src/main/java/com/test/usersubscriptionsservice/repository/SubscriptionRepository.java
@@ -1,7 +1,10 @@
 package com.test.usersubscriptionsservice.repository;
 
 import com.test.usersubscriptionsservice.core.model.Subscription;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,4 +12,11 @@ import java.util.List;
 @Repository
 public interface SubscriptionRepository extends JpaRepository<Subscription, Long> {
     List<Subscription> findAllByUserId(Long userId);
+
+    Page<Subscription> findAllByUserId(Long userId, Pageable pageable);
+
+    @Query("SELECT s.serviceType AS serviceName, COUNT(s.user) AS userCount " +
+            "FROM Subscription s GROUP BY s.serviceType ORDER BY COUNT(s.user) DESC")
+    List<Object[]> findTopSubscriptionsRaw();
+
 }
