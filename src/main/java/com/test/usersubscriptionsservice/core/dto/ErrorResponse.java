f71336116a434e73fb5af03777fd7a7ddd8f3083
Ivanov Ilya <iiilya12@yandex.ru>
2025-05-08 13:48:15 +0300

    feat: add dto's

---
+++ src/main/java/com/test/usersubscriptionsservice/core/dto/ErrorResponse.java
@@ -0,0 +1,5 @@
+package com.test.usersubscriptionsservice.core.dto;
+
+public record ErrorResponse(String code, String message) {
+}
+
---
+++ src/main/java/com/test/usersubscriptionsservice/core/dto/UserDTO.java
@@ -0,0 +1,8 @@
+package com.test.usersubscriptionsservice.core.dto;
+
+public record UserDTO(
+        Long id,
+        String username,
+        String email
+) {
+}
