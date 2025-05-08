b736b82b0270bed043e9540532e3738ab9f80475
Ivanov Ilya <iiilya12@yandex.ru>
2025-05-08 12:10:12 +0300

    load structure

---
+++ src/main/java/com/test/usersubscriptionsservice/UserSubscriptionsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.test.usersubscriptionsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserSubscriptionsServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserSubscriptionsServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/test/usersubscriptionsservice/UserSubscriptionsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.usersubscriptionsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserSubscriptionsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
