4bf4b4ebda48813752b5947b996453d48c29cd23
Ivanov Ilya <iiilya12@yandex.ru>
2025-05-08 19:38:49 +0300

    feat: add dto validation

--- src/main/java/com/test/usersubscriptionsservice/core/dto/SubscriptionAdditionDTO.java
+++ src/main/java/com/test/usersubscriptionsservice/core/dto/SubscriptionAdditionDTO.java
@@ -1,11 +1,22 @@
 package com.test.usersubscriptionsservice.core.dto;
 
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
 import java.time.LocalDateTime;
 
 public record SubscriptionAdditionDTO(
+        @NotBlank(message = "Service type must not be blank")
         String serviceType,
+
+        @NotNull(message = "Start date must not be null")
         LocalDateTime startDate,
+
+        @NotNull(message = "Expire date must not be null")
+        @Future(message = "Expire date must be in the future")
         LocalDateTime expireDate
 ) {
+
 }
